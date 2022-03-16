90406d45485da6b0e0df03a7a7ea499e8b8420e7
azrielrisywan <azrielrisywan@gmail.com>
2022-03-16 13:32:21 +0700

    migration file

---
+++ database/migrations/2022_03_16_061249_create_buku_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buku', function (Blueprint $table) {
+            $table->string('isbn')->primary();
+            $table->string('judul');
+            $table->string('kategori');
+            $table->string('tingkatan');
+            $table->string('gambar');
+            $table->string('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buku');
+    }
+};
