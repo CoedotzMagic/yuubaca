37d23552260bfe10b2275b8c59cb2249819096fd
azrielrisywan <azrielrisywan@gmail.com>
2022-03-19 10:24:45 +0700

    rename user to pustakawan

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,11 +39,7 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $pustakawan = Pustakawan::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        $pustakawan =
 
         event(new Registered($pustakawan));
 
