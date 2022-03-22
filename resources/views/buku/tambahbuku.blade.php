5052d7854717a41420da661a87ab1c903e071c60
poncoe <poncoe4mac@gmail.com>
2022-03-23 01:03:59 +0700

    added error handling in tambah buku

--- resources/views/buku/tambahbuku.blade.php
+++ resources/views/buku/tambahbuku.blade.php
@@ -9,10 +9,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <strong>Waduh!</strong> Kayaknya ada masalah nih dengan inputannya, coba di cek!.<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+
                     <form method="post" action="{{ route('tambah.buku') }}">
                         <x-validation-errors class="mb-4" :errors="$errors" />
                         @method('PUT')
                         @csrf
+
                         <div class="form-row">
                             <div class="col-md-4 mb-3">
                                 <label for="isbn">ISBN</label>
