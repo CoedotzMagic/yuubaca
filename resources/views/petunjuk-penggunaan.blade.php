44545b2dafc28380e994ee53a2a0513cc1a3e089
poncoe <poncoe4mac@gmail.com>
2022-03-22 22:16:24 +0700

    create func petunjuk penggunaan

---
+++ resources/views/petunjuk-penggunaan.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Web Icon -->
+    <link rel="icon" href="(BELUM DIISI)" type="image/icon type">
+
+    <!-- Title Web -->
+    <title>YuuBaca : Petunjuk Penggunaan</title>
+
+    <!-- Style -->
+
+  </head>
+  <body>
+
+  <x-app-layout>
+    <x-slot name="header">
+
+        <!-- PDF -->
+        <object data="/petunjuk-penggunaan-yuubaca.pdf" type="application/pdf" width="100%" height="800px"> 
+        <p>Yah, karena browser ini tidak dilengkapi plugin, jadi alternatifnya anda bisa <a href="/petunjuk-penggunaan-yuubaca.pdf">Klik untuk mengunduh petunjuk penggunaan YuuBaca.</a></p>  
+        </object>
+        <!-- PDF -->
+
+    </x-slot>
+
+    
+
+</x-app-layout>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  </body>
+</html>
