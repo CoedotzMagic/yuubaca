e14106da490eb1f2087d74c4e3277ca004954c96
poncoe <poncoe4mac@gmail.com>
2022-05-01 06:17:36 +0700

    improving searching

--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -10,7 +10,7 @@
         <div class="pull-left">
             <form action="{{ url()->current() }}" method="get">
                 <div class="relative mx-auto">
-                    <input type="search" name="keyword" value="{{ request('keyword') }}" placeholder="Cari Judul ....." class="block w-full pl-4 pr-10 text-sm leading-5 transition rounded-full shadow-sm border-secondary-300 bg-secondary-50 focus:bg-white focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
+                    <input type="search" name="keyword" value="{{ request('keyword') }}" placeholder="Cari Judul atau ISBN ..." class="block w-full pl-4 pr-10 text-sm leading-5 transition rounded-full shadow-sm border-secondary-300 bg-secondary-50 focus:bg-white focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
                     <button type="submit" class="absolute top-0 right-0 inline-flex items-center py-2 text-sm focus:outline-none">
                         <svg class="w-5 h-5 text-gray-500 transition dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 disabled:opacity-25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
