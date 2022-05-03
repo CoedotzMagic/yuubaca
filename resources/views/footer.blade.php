db773927484de52fbfc49df2eb8635d50466a0c4
poncoe <poncoe4mac@gmail.com>
2022-05-03 21:36:23 +0700

    improvment footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 px-3 border-top">
     <p class="col-md-4 mb-0 text-muted">&copy; 2022 Stand Here Alone, Inc</p>
 
-    <a class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+    <!-- <a class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
     <p class="col-md-4 mb-0 text-muted">YuuBaca</p>
     </a>
 
@@ -9,5 +9,5 @@
     <li class="nav-item"><a href="dashboard" class="nav-link px-2 text-muted">Home</a></li>
     <li class="nav-item"><a href="feature" class="nav-link px-2 text-muted">Features</a></li>
     <li class="nav-item"><a href="about" class="nav-link px-2 text-muted">About</a></li>
-    </ul>
+    </ul> -->
 </footer>
