c2aa131f64e729572d48fee0292901ea0c58f423
poncoe <poncoe4mac@gmail.com>
2022-05-14 00:27:49 +0700

    improving forgot password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -7,20 +7,20 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('Lupa Kata Sandi? Tidak masalah! Silahkan hubungi Admin YuuBaca ke email admin@yuubaca.com dengan mengirimkan Nama & Email anda, Subjek Pesan : Lupa Kata Sandi') }}
         </div>
 
-        <!-- Session Status -->
+         <!--  Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+        <!-- <form method="POST" action="{{ route('password.email') }}">
+            @csrf -->
 
             <!-- Email Address -->
-            <div>
+            <!-- <div>
                 <x-label for="email" :value="__('Email')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
@@ -31,6 +31,6 @@
                     {{ __('Email Password Reset Link') }}
                 </x-button>
             </div>
-        </form>
+        </form> -->
     </x-auth-card>
 </x-guest-layout>
